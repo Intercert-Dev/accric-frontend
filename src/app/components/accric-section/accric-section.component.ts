15e0be175284f90b0ba75de82bd919896cf0d1d2
Ashutosh Rai <ashutosh@intercert.com>
2025-11-25 16:56:13 +0530

    add bg-colour in accric section

--- src/app/components/accric-section/accric-section.component.ts
+++ src/app/components/accric-section/accric-section.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 @Component({
   selector: 'app-accric-section',
-  imports: [],
+  imports: [ RouterModule, RouterLink, RouterLinkActive],
   templateUrl: './accric-section.component.html',
   styleUrl: './accric-section.component.scss',
 })
