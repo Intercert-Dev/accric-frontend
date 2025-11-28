9212184a7381d8895be3d13d222016ed11895774
Ashutosh Rai <ashutosh@intercert.com>
2025-11-28 17:55:46 +0530

    add link in pci dss, modify the size of icon

--- src/app/pages/pci-dss/pci-dss.component.ts
+++ src/app/pages/pci-dss/pci-dss.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-pci-dss',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './pci-dss.component.html',
   styleUrl: './pci-dss.component.scss',
 })
