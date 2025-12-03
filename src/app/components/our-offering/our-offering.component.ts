73511e832e71b8866195281350d830d8c8e8931c
Ashutosh Rai <ashutosh@intercert.com>
2025-12-03 11:01:08 +0530

    add text to services and link to pci dss offering section

--- src/app/components/our-offering/our-offering.component.ts
+++ src/app/components/our-offering/our-offering.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-our-offering',
+  imports: [
+    RouterLink
+  ],
   standalone: true,
   templateUrl: './our-offering.component.html',
   styleUrl: './our-offering.component.scss',
